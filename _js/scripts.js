8eddb704c2aed5c8612b2a3f34e48600886ece7f
Noyllopa <13807560+Noyllopa@users.noreply.github.com>
2018-03-08 11:24:36 +0800

    邮箱修改

--- _js/scripts.js
+++ _js/scripts.js
@@ -242,7 +242,7 @@ $( "#contactForm" ).submit( function( e ) {
     $.ajax( {
 
         // Change the email address here:
-        url: "https://formspree.io/jan.czizikow@Fmail.com",
+        url: "https://formspree.io/noyllopa.rain@gmail.com",
         method: "POST",
         data: $( this ).serialize(),
         dataType: "json",
